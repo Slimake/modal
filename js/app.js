d049082bac2b1279348cc53c858156fc807e8abb
Ajakaiye Afeso <marcusitovie@gmail.com>
2021-03-13 23:48:51 +0100

    Add initial file

--- js/app.js
+++ js/app.js
@@ -1,98 +1,98 @@
-// Work to get the filter buttons working
-(function(){
-    // const buttons = document.querySelectorAll('.btn')
-    // const storeItems = document.querySelectorAll('.store-item')
-    //console.log(buttons)
-    // buttons.forEach(function(button){
-    //     button.addEventListener('click', function(e){
-    //         //prevent the default link jump to top of page
-    //         e.preventDefault()
-    //         //grab the filter button that was clicked
-    //         const filter = e.target.dataset.filter
-    //         if (filter === 'all'){
-    //             //show all items
-    //             storeItems.forEach(function(item){
-    //                 item.style.display = 'block'
-    //             })
-    //         } else if (filter === 'cakes'){
-    //             storeItems.forEach(function(item){
-    //                 if (item.classList.contains('cakes')){
-    //                     item.style.display = 'block'
-    //                 } else {
-    //                     item.style.display = 'none'
-    //                 }
-    //             })
-    //         } else if (filter === 'cupcakes'){
-    //             storeItems.forEach(function(item){
-    //                 if (item.classList.contains('cupcakes')){
-    //                     item.style.display = 'block'
-    //                 } else {
-    //                     item.style.display = 'none'
-    //                 }
-    //             })
-    //         } else if (filter === 'sweets'){
-    //             storeItems.forEach(function(item){
-    //                 if (item.classList.contains('sweets')){
-    //                     item.style.display = 'block'
-    //                 } else {
-    //                     item.style.display = 'none'
-    //                 }
-    //             })
-    //         } else if (filter === 'doughnuts'){
-    //             storeItems.forEach(function(item){
-    //                 if (item.classList.contains('doughnuts')){
-    //                     item.style.display = 'block'
-    //                 } else {
-    //                     item.style.display = 'none'
-    //                 }
-    //             })
-    //         }
-    //     })
-    // })
-
-    // refactor to get rid of DRY code
-    const buttons = document.querySelectorAll('.btn')
-    const storeItems = document.querySelectorAll('.store-item')
-
-    buttons.forEach((button)=> {
-        button.addEventListener('click', (e) => {
-            e.preventDefault()
-            const filter = e.target.dataset.filter
-            
-            storeItems.forEach((item)=> {
-                if (filter === 'all'){
-                    item.style.display = 'block'
-                } else {
-                    if (item.classList.contains(filter)){
-                        item.style.display = 'block'
-                    } else {
-                        item.style.display = 'none'
-                    }
-                }
-            })
-        })
-    })
-
+(function() {
+    let cartInfo = document.querySelector(".cart-info");
+    let cart = document.querySelector(".cart");
+    
+    cartInfo.addEventListener("click", () => {
+        cart.classList.toggle("show-cart");
+    });
 })();
 
-//wire up filter search box
-(function(){
 
-    const searchBox = document.querySelector('#search-item')
-    const storeItems = document.querySelectorAll('.store-item')
+(function() {
+    const buttons = document.querySelectorAll(".filter-btn");
+    const storeItems = document.querySelectorAll(".store-item")
 
-    searchBox.addEventListener('keyup', (e) => {
-    
-        const searchFilter = e.target.value.toLowerCase().trim()
-        //display only items that contain filter input
+    buttons.forEach((button) => {
+        button.addEventListener("click", (e) => {
 
+            // prevent link from going to top of the page after click
+            e.preventDefault();
+            const buttonFilter = e.target.textContent.toLowerCase().trim();
+
+            // button filter
+            storeItems.forEach((item) => {
+                if (buttonFilter === "all") {
+                    item.style.display = "block";
+                } else {
+                    if ( item.classList.contains(buttonFilter) ) {
+                        item.style.display = "block";
+                    } else {
+                        item.style.display = "none";
+                    }
+                }
+            });
+        });
+    });
+})();
+
+(function() {
+    const search = document.querySelector("#search-item");
+    const storeItems = document.querySelectorAll(".store-item");
+
+    search.addEventListener("input", (e) => {
+        const searchFilter = e.target.value.toLowerCase().trim();
+        
+        // search field filter
         storeItems.forEach((item) => {
-            if (item.textContent.includes(searchFilter)){
-                item.style.display = 'block'
+            if ( item.textContent.includes(searchFilter) ) {
+                item.style.display = "block";
             } else {
-                item.style.display = 'none'
+                item.style.display = "none";
             }
-        })
-    })
+        });
+    });
+})();
 
+(function() {
+    const storeImages = document.querySelectorAll(".store-img");
+    const overlay = document.querySelector(".overlay")
+    const hideOverlay = document.querySelector(".hide-overlay");
+    const overlayImage = document.querySelector(".overlay-img")
+    const body = document.querySelector("body");
+    const prevBtn = document.querySelector(".prev-btn");
+    const nextBtn = document.querySelector(".next-btn");
+    const imageArr = [];
+    let count;
+    
+    storeImages.forEach((image, index) => {
+        imageArr.push(image.src);
+        
+        image.addEventListener("click", (e) => {
+            count = index;
+            overlay.classList.add("show-overlay");
+            overlayImage.src = e.target.src;
+            body.style.overflowY = "hidden";
+        });
+    });
+
+    hideOverlay.addEventListener("click", () => {
+        overlay.classList.remove("show-overlay");
+        body.style.overflowY = "visible";
+    });
+    
+    prevBtn.addEventListener("click", () => {
+        count--;
+        if (count < 0) {
+            count = imageArr.length - 1;
+        }
+        overlayImage.src = imageArr[count];
+    });
+    
+    nextBtn.addEventListener("click", () => {
+        count++;
+        if (count >= imageArr.length) {
+            count = 0;
+        }
+        overlayImage.src = imageArr[count];
+    });
 })();
